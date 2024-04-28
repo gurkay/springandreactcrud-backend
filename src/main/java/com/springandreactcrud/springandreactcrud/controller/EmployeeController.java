fe1a5b24544b3c51d1b889394d6fadc942a2360e
gurkay <g_basyigit@hotmail.com>
2024-04-28 09:35:29 +0300

    new codes

--- src/main/java/com/springandreactcrud/springandreactcrud/controller/EmployeeController.java
+++ src/main/java/com/springandreactcrud/springandreactcrud/controller/EmployeeController.java
@@ -3,6 +3,7 @@ package com.springandreactcrud.springandreactcrud.controller;
 import java.util.List;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -17,6 +18,7 @@ import com.springandreactcrud.springandreactcrud.service.IEmployeeService;
 
 import lombok.AllArgsConstructor;
 
+@CrossOrigin("*")
 @AllArgsConstructor
 @RestController
 @RequestMapping("/api/v1/employees")
